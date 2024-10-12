56ba7b8c5de7cabf298cddfa3fd7e5164df74e45
DamienCKj2812 <damienckj5993@gmail.com>
2024-10-12 21:42:17 +0800

    Add the User class, and basic admin operations

---
+++ JavaApplication7/src/constants/Constants.java
@@ -0,0 +1,6 @@
+package constants;
+
+public class Constants {
+
+    public static final String USER_DATA_PATH = "data/UserData.txt";
+}
--- JavaApplication7/src/models/User.java
+++ JavaApplication7/src/models/User.java
@@ -1,13 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package models;
 
-/**
- *
- * @author Aorus
- */
 public class User {
-    
+
+    private String userID;
+    private String username;
+    private String password;
+    private String role;
+
+    public User(String userID, String username, String password, String role) {
+        this.userID = userID;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void displayUserInfo() {
+        System.out.println("User ID: " + userID);
+        System.out.println("Username: " + username);
+        System.out.println("Role: " + role);
+    }
+
 }
