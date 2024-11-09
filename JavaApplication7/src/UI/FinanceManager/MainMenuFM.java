1acca4d479f49965c69d3ae959fb250707c1a310
Henry Tham <winghein5778@gmail.com>
2024-11-10 01:50:33 +0800

    Improvements for FM Main Menu

--- JavaApplication7/src/UI/FinanceManager/MainMenuFM.java
+++ JavaApplication7/src/UI/FinanceManager/MainMenuFM.java
@@ -26,49 +26,156 @@ public class MainMenuFM extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        panel1 = new java.awt.Panel();
+        MainMenuFM_CSS = new javax.swing.JButton();
+        MainMenuFM_VPO = new javax.swing.JButton();
+        MainMenuFM_MP = new javax.swing.JButton();
+        MainMenuFM_SPS = new javax.swing.JButton();
+        MainMenuFM_LO = new javax.swing.JButton();
         MainMenuFM_greet1 = new javax.swing.JLabel();
         MainMenuFM_username = new javax.swing.JLabel();
         MainMenuFM_greet2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(153, 204, 255));
 
-        MainMenuFM_greet1.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        MainMenuFM_CSS.setBackground(new java.awt.Color(169, 188, 208));
+        MainMenuFM_CSS.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 18)); // NOI18N
+        MainMenuFM_CSS.setForeground(new java.awt.Color(27, 27, 30));
+        MainMenuFM_CSS.setText("Check Stock Status");
+        MainMenuFM_CSS.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                MainMenuFM_CSSActionPerformed(evt);
+            }
+        });
+
+        MainMenuFM_VPO.setBackground(new java.awt.Color(216, 219, 226));
+        MainMenuFM_VPO.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 18)); // NOI18N
+        MainMenuFM_VPO.setForeground(new java.awt.Color(55, 63, 81));
+        MainMenuFM_VPO.setText("View Purchase Order");
+        MainMenuFM_VPO.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                MainMenuFM_VPOActionPerformed(evt);
+            }
+        });
+
+        MainMenuFM_MP.setBackground(new java.awt.Color(88, 164, 176));
+        MainMenuFM_MP.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 18)); // NOI18N
+        MainMenuFM_MP.setForeground(new java.awt.Color(55, 63, 81));
+        MainMenuFM_MP.setText("Make Payment");
+        MainMenuFM_MP.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                MainMenuFM_MPActionPerformed(evt);
+            }
+        });
+
+        MainMenuFM_SPS.setBackground(new java.awt.Color(55, 63, 81));
+        MainMenuFM_SPS.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 18)); // NOI18N
+        MainMenuFM_SPS.setForeground(new java.awt.Color(88, 164, 176));
+        MainMenuFM_SPS.setText("Supplier Payment Status");
+        MainMenuFM_SPS.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                MainMenuFM_SPSActionPerformed(evt);
+            }
+        });
+
+        MainMenuFM_LO.setBackground(new java.awt.Color(255, 75, 62));
+        MainMenuFM_LO.setFont(new java.awt.Font("Segoe UI Semibold", 0, 14)); // NOI18N
+        MainMenuFM_LO.setForeground(new java.awt.Color(255, 229, 72));
+        MainMenuFM_LO.setText("Log Out");
+
+        MainMenuFM_greet1.setFont(new java.awt.Font("Segoe UI Historic", 0, 24)); // NOI18N
+        MainMenuFM_greet1.setForeground(new java.awt.Color(55, 63, 81));
         MainMenuFM_greet1.setText("Good day,");
 
         MainMenuFM_username.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        MainMenuFM_username.setForeground(new java.awt.Color(88, 164, 176));
         MainMenuFM_username.setText("Username");
 
-        MainMenuFM_greet2.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        MainMenuFM_greet2.setFont(new java.awt.Font("MV Boli", 1, 18)); // NOI18N
+        MainMenuFM_greet2.setForeground(new java.awt.Color(59, 59, 59));
         MainMenuFM_greet2.setText("Welcome back to Finance Manager Dashboard");
+
+        javax.swing.GroupLayout panel1Layout = new javax.swing.GroupLayout(panel1);
+        panel1.setLayout(panel1Layout);
+        panel1Layout.setHorizontalGroup(
+            panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(panel1Layout.createSequentialGroup()
+                        .addComponent(MainMenuFM_greet1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(MainMenuFM_username))
+                    .addComponent(MainMenuFM_greet2)
+                    .addGroup(panel1Layout.createSequentialGroup()
+                        .addGap(82, 82, 82)
+                        .addGroup(panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(panel1Layout.createSequentialGroup()
+                                .addComponent(MainMenuFM_VPO)
+                                .addGap(113, 113, 113)
+                                .addComponent(MainMenuFM_CSS))
+                            .addGroup(panel1Layout.createSequentialGroup()
+                                .addComponent(MainMenuFM_MP)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(MainMenuFM_SPS)))))
+                .addContainerGap(49, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(MainMenuFM_LO, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+        panel1Layout.setVerticalGroup(
+            panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(MainMenuFM_greet1)
+                    .addComponent(MainMenuFM_username))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(MainMenuFM_greet2)
+                .addGap(54, 54, 54)
+                .addGroup(panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(MainMenuFM_CSS, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(MainMenuFM_VPO, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(MainMenuFM_MP, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(MainMenuFM_SPS, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 43, Short.MAX_VALUE)
+                .addComponent(MainMenuFM_LO, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(MainMenuFM_greet1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(MainMenuFM_username))
-                    .addComponent(MainMenuFM_greet2))
-                .addContainerGap(246, Short.MAX_VALUE))
+            .addComponent(panel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(MainMenuFM_greet1)
-                    .addComponent(MainMenuFM_username))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(MainMenuFM_greet2)
-                .addContainerGap(331, Short.MAX_VALUE))
+            .addComponent(panel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void MainMenuFM_VPOActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_MainMenuFM_VPOActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_MainMenuFM_VPOActionPerformed
+
+    private void MainMenuFM_CSSActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_MainMenuFM_CSSActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_MainMenuFM_CSSActionPerformed
+
+    private void MainMenuFM_MPActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_MainMenuFM_MPActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_MainMenuFM_MPActionPerformed
+
+    private void MainMenuFM_SPSActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_MainMenuFM_SPSActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_MainMenuFM_SPSActionPerformed
 
     /**
      * @param args the command line arguments
@@ -106,8 +213,14 @@ public class MainMenuFM extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton MainMenuFM_CSS;
+    private javax.swing.JButton MainMenuFM_LO;
+    private javax.swing.JButton MainMenuFM_MP;
+    private javax.swing.JButton MainMenuFM_SPS;
+    private javax.swing.JButton MainMenuFM_VPO;
     private javax.swing.JLabel MainMenuFM_greet1;
     private javax.swing.JLabel MainMenuFM_greet2;
     private javax.swing.JLabel MainMenuFM_username;
+    private java.awt.Panel panel1;
     // End of variables declaration//GEN-END:variables
 }
