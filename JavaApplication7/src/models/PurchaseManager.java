6920d94031f5595724056ecf98ac27aeefa6be65
DamienCKj2812 <damienckj5993@gmail.com>
2024-11-14 11:48:31 +0800

    add more features for the sales and purchase manager

--- JavaApplication7/src/models/PurchaseManager.java
+++ JavaApplication7/src/models/PurchaseManager.java
@@ -4,42 +4,31 @@
  */
 package models;
 
-import constants.Constants;
-import utils.FileManager;
 import java.io.IOException;
 import java.util.List;
-import java.util.ArrayList;
-import java.util.NoSuchElementException;
 
 public class PurchaseManager extends User {
 
+    private SupplierAction supplierAction = new SupplierAction();
     private RequisitionAction requisitionAction = new RequisitionAction();
     private PurchaseOrderAction purchaseOrderAction = new PurchaseOrderAction();
+    private Inventory inventory = new Inventory();
 
     public PurchaseManager(String userID, String username, String password) {
         super(userID, username, password, "purchaseManager");
     }
 
-    public List<PurchaseOrder> getAllPurchaseOrders() throws IOException {
-        return purchaseOrderAction.getAllPurchaseOrders();
+    // List of Items (View)
+    public List<Item> getInventoryItems() throws IOException {
+        return inventory.getInventoryItems();
     }
 
-    public PurchaseOrder getPurchaseOrder(String requisitionId) throws IOException {
-        return purchaseOrderAction.getPurchaseOrder(requisitionId);
+    // List of Suppliers (View)
+    public List<Supplier> getAllSupplier() throws IOException {
+        return supplierAction.getAllSupplier();
     }
 
-    public PurchaseOrder removePurchaseOrder(String requisitionId) throws IOException {
-        return purchaseOrderAction.removePurchaseOrder(requisitionId);
-    }
-
-    public PurchaseOrder addPurchaseOrder(String newRequisitionID, String newItemId, String newOrderQuantity, String newOrderDate, String newExpectedDeliveryDate, String newStatus, String newPurchaseManagerID) throws IOException {
-        return purchaseOrderAction.addPurchaseOrder(newRequisitionID, newItemId, newOrderQuantity, newOrderDate, newExpectedDeliveryDate, newStatus, newPurchaseManagerID);
-    }
-
-    public PurchaseOrder updatePurchaseOrder(String purchaseOrderId, String newRequisitionID, String newItemId, String newOrderQuantity, String newOrderDate, String newExpectedDeliveryDate, String newStatus, String newPurchaseManagerID) throws IOException {
-        return purchaseOrderAction.updatePurchaseOrder(purchaseOrderId, newRequisitionID, newItemId, newOrderQuantity, newOrderDate, newExpectedDeliveryDate, newStatus, newPurchaseManagerID);
-    }
-
+    // Requisitions part
     public List<Requisition> getAllRequisitions() throws IOException {
         return requisitionAction.getAllRequisitions();
     }
@@ -58,6 +47,27 @@ public class PurchaseManager extends User {
 
     public Requisition updateRequisition(String requisitionId, String newItemId, String newQuantity, String newRequiredDate, String newSalesManagerId, String newRequisitionDate, String newStatus) throws IOException {
         return requisitionAction.updateRequisition(requisitionId, newItemId, newQuantity, newRequiredDate, newSalesManagerId, newRequisitionDate, newStatus);
+    }
+
+    // Purchase orders part
+    public List<PurchaseOrder> getAllPurchaseOrders() throws IOException {
+        return purchaseOrderAction.getAllPurchaseOrders();
+    }
+
+    public PurchaseOrder getPurchaseOrder(String requisitionId) throws IOException {
+        return purchaseOrderAction.getPurchaseOrder(requisitionId);
+    }
+
+    public PurchaseOrder removePurchaseOrder(String requisitionId) throws IOException {
+        return purchaseOrderAction.removePurchaseOrder(requisitionId);
+    }
+
+    public PurchaseOrder addPurchaseOrder(String newRequisitionID, String newItemId, String newOrderQuantity, String newOrderDate, String newExpectedDeliveryDate, String newStatus, String newPurchaseManagerID) throws IOException {
+        return purchaseOrderAction.addPurchaseOrder(newRequisitionID, newItemId, newOrderQuantity, newOrderDate, newExpectedDeliveryDate, newStatus, newPurchaseManagerID);
+    }
+
+    public PurchaseOrder updatePurchaseOrder(String purchaseOrderId, String newRequisitionID, String newItemId, String newOrderQuantity, String newOrderDate, String newExpectedDeliveryDate, String newStatus, String newPurchaseManagerID) throws IOException {
+        return purchaseOrderAction.updatePurchaseOrder(purchaseOrderId, newRequisitionID, newItemId, newOrderQuantity, newOrderDate, newExpectedDeliveryDate, newStatus, newPurchaseManagerID);
     }
 
 }
--- JavaApplication7/src/models/SalesManager.java
+++ JavaApplication7/src/models/SalesManager.java
@@ -5,13 +5,21 @@ import java.util.List;
 
 public class SalesManager extends User {
 
-    SalesAction salesAction = new SalesAction();
-    RequisitionAction requisitionAction = new RequisitionAction();
+    private SalesAction salesAction = new SalesAction();
+    private RequisitionAction requisitionAction = new RequisitionAction();
+    private Inventory inventory = new Inventory();
+    private PurchaseOrderAction purchaseOrderAction = new PurchaseOrderAction();
 
     public SalesManager(String userID, String username, String password) {
         super(userID, username, password, "salesManager");
     }
 
+    // List of Items (View)
+    public List<Item> getInventoryItems() throws IOException {
+        return inventory.getInventoryItems();
+    }
+
+    // Sales part
     public double getSalesRevenue(Sales salesEntry) throws IOException {
         return salesAction.getSalesRevenue(salesEntry);
     }
@@ -36,8 +44,14 @@ public class SalesManager extends User {
         return salesAction.updateSalesEntry(salesID, itemID, quantitySold, dateSold, notes);
     }
 
+    // Create Requisition 
     public Requisition addRequisition(String newItemId, String newQuantity, String newRequiredDate, String newSalesManagerId, String newRequisitionDate) throws IOException {
         return requisitionAction.addRequisition(newItemId, newQuantity, newRequiredDate, newSalesManagerId, newRequisitionDate, "Pending");
+    }
+
+    // List of Purchaser Orders (View)
+    public List<PurchaseOrder> getAllPurchaseOrders() throws IOException {
+        return purchaseOrderAction.getAllPurchaseOrders();
     }
 
 }
